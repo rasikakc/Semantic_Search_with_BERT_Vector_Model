433082dcab5d5e4aa742b02d9a4edae4a2667589
Rasika Chinchole <38551946+rasikakc@users.noreply.github.com>
2026-07-04 17:30:51 +0530

    Initial commit of BERT vector model code

---
+++ searchApp.py
@@ -0,0 +1,71 @@
+import streamlit as st
+from elasticsearch import Elasticsearch
+from sentence_transformers import SentenceTransformer
+
+indexName = "all_products"
+
+try:
+    es = Elasticsearch(
+    "https://be2d2d7c8a5a4f02ad13d92a6d1e133e.asia-south1.gcp.elastic-cloud.com:443",
+    basic_auth=("elastic","1LweND5dy6PbB4OXARnJxCXx")
+    )
+except ConnectionError as e:
+    print("Connection Error:", e)
+    
+if es.ping():
+    print("Succesfully connected to ElasticSearch!!")
+else:
+    print("Oops!! Can not connect to Elasticsearch!")
+
+
+
+
+def search(input_keyword):
+    model = SentenceTransformer('all-mpnet-base-v2')
+    vector_of_input_keyword = model.encode(input_keyword).tolist()
+
+    query = {
+        "field": "DescriptionVector",
+        "query_vector": vector_of_input_keyword,
+        "k": 10,
+        "num_candidates": 500
+    }
+    res = es.search(index="all_products"
+                        , knn=query 
+                        , source=["ProductName","Description"]
+                        )
+    results = res["hits"]["hits"]
+
+    return results
+
+def main():
+    st.title("Search Myntra Fashion Products")
+
+    # Input: User enters search query
+    search_query = st.text_input("Enter your search query")
+
+    # Button: User triggers the search
+    if st.button("Search"):
+        if search_query:
+            # Perform the search and get results
+            results = search(search_query)
+
+            # Display search results
+            st.subheader("Search Results")
+            for result in results:
+                with st.container():
+                    if '_source' in result:
+                        try:
+                            st.header(f"{result['_source']['ProductName']}")
+                        except Exception as e:
+                            print(e)
+                        
+                        try:
+                            st.write(f"Description: {result['_source']['Description']}")
+                        except Exception as e:
+                            print(e)
+                        st.divider()
+
+                    
+if __name__ == "__main__":
+    main()
